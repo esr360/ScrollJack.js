8138c594c421d8b7f0a17df116c6e735f4780fbe
Edmund Reed <esr360@live.com>
2016-12-12 15:01:36 +0000

    aligning with repo name

--- ScrollJack.js
+++ ScrollJack.js
@@ -1,64 +1,51 @@
-// Custom scrolling speed with jQuery
-// Source: github.com/ByNathan/jQuery.scrollSpeed
-// Version: 1.0
-
+// ScrollJack.js
+// Forked from: github.com/ByNathan/jQuery.scrollSpeed
 (function($) {
-    
+
     jQuery.scrollSpeed = function(step, speed) {
-        
-        var $document = $(document),
-        
-            $window = $(window),
-            
-            $body = $('html, body'),
-            
-            viewport = $window.height(),
-            
-            top = 0,
-            
-            scroll = false;
-            
-        if (window.navigator.msPointerEnabled)
-        
+
+        var $document = $(document);
+        var $window = $(window);
+        var $body = $('html, body');
+        var viewport = $window.height();
+        var top = 0;
+        var scroll = false;
+
+        if (window.navigator.msPointerEnabled) {
             return false;
-            
+        }
+
         $window.on('mousewheel DOMMouseScroll', function(e) {
-        
+
             scroll = true;
             
-            if (e.originalEvent.wheelDeltaY < 0 || e.originalEvent.detail > 0)
-            
+            if (e.originalEvent.wheelDeltaY < 0 || e.originalEvent.detail > 0) {
                 top = (top + viewport) >= $document.height() ? top : top += step;
-                
-            if (e.originalEvent.wheelDeltaY > 0 || e.originalEvent.detail < 0)
-            
+            }
+
+            if (e.originalEvent.wheelDeltaY > 0 || e.originalEvent.detail < 0) {
                 top = top <= 0 ? 0 : top -= step;
-                
+            }
+
             $body.stop().animate({
-            
                 scrollTop: top
-                
             }, speed, 'default', function() {
-            
                 scroll = false;
-                
             });
-            
+
             return false;
-            
+
         }).on('scroll', function() {
-        
-            if (!scroll) top = $window.scrollTop();
-            
-        }).on('resize', function() {
-            
+            if (!scroll) {
+                top = $window.scrollTop();
+            }
+        }).on('resize', function() { 
             viewport = $window.height();
-            
-        });       
+        });
+
     };
-    
+
     jQuery.easing.default = function (x,t,b,c,d) {
-    
         return -c * ((t=t/d-1)*t*t*t - 1) + b;
     };
     
